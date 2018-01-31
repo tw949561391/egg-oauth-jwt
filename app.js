2c0864908cc73c16c4430f5bad9691fbf8577441
miup <tw949561391@icloud.com>
2018-01-31 22:20:14 +0800

    ok

--- app.js
+++ app.js
@@ -2,22 +2,26 @@
 
 const path = require('path');
 const OAuth2Server = require('./lib/server');
+const Jwt = require('jsonwebtoken');
+const util = require('util');
 
 module.exports = app => {
-  app.coreLogger.info('[egg-oauth-jwt] init begin');
-
-  const config = app.config.oauthJwt;
-  const extendModelFileName = config.extend || 'oauth';
-  let Model = app.loader.loadFile(path.join(app.config.baseDir, `app/extend/${extendModelFileName}.js`));
-
-  if (Model === null) {
-    Model = app.loader.loadFile('./lib/default-model.js');
-    app.coreLogger.warn('[egg-oauth-jwt]  use default oauthModel');
-  }
-  try {
-    app.oauthJwt = new OAuth2Server(config, new Model(app));
-    app.coreLogger.info('[egg-oauth-jwt] init success');
-  } catch (e) {
-    app.coreLogger.error('[egg-oauth-jwt] init error, %s', e.message);
-  }
+    app.coreLogger.info('[egg-oauth-jwt] init begin');
+    app.Jwt = Jwt;
+    const config = app.config.oauthJwt;
+    const extendModelFileName = config.extend || 'oauth';
+    const BaseModel = app.loader.loadFile(path.join(__dirname, './lib/default-model.js'));
+    let Model = app.loader.loadFile(path.join(app.config.baseDir, `app/extend/${extendModelFileName}.js`));
+    if (Model) {
+        util.inherits(Model, BaseModel);
+    } else {
+        Model = BaseModel;
+    }
+    try {
+        const model = new Model(app);
+        app.oauthJwt = new OAuth2Server(config, model);
+        app.coreLogger.info('[egg-oauth-jwt] init success');
+    } catch (e) {
+        app.coreLogger.error('[egg-oauth-jwt] init error, %s', e.message);
+    }
 };

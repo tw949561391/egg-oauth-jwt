099f4fa33d0b5a86186a152da0f3a208d0d1abd6
Tristan <tw949561391@hotmail.com>
2018-07-15 12:23:40 +0800

    添加auth_code

--- app.js
+++ app.js
@@ -2,15 +2,13 @@
 
 const path = require('path');
 const OAuth2ServerBuilder = require('./lib/server');
-const Jwt = require('jsonwebtoken');
 
 module.exports = app => {
-  app.coreLogger.info('[egg-oauth-jwt] init begin');
-  app.Jwt = Jwt;
-  const config = app.config.oauthJwt;
-  const extendModelFileName = config.extend || 'oauth';
-  const Model = require(path.join(app.config.baseDir, `app/extend/${extendModelFileName}.js`));
-  const model = new Model(app);
-  app.oauthJwt = new OAuth2ServerBuilder(config, model, app.logger);
-  app.coreLogger.info('[egg-oauth-jwt] init success');
+    app.coreLogger.info('[egg-oauth-jwt] init begin');
+    const config = app.config.oauthJwt;
+    const extendModelFileName = config.extend || 'oauth';
+    const Model = require(path.join(app.config.baseDir, `app/extend/${extendModelFileName}.js`));
+    const model = new Model(app);
+    app.oauthJwt = new OAuth2ServerBuilder(config, model, app.coreLogger);
+    app.coreLogger.info('[egg-oauth-jwt] init success');
 };

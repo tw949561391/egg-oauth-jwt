beafc14229fb4b197677912367cfb751e70c46ff
j0 <tw949561391@hotmail.com>
2018-08-24 16:03:22 +0800

    scopes->scope

--- app.js
+++ app.js
@@ -9,11 +9,13 @@ module.exports = app => {
     const config = app.config.oauthjwt;
     const extendModelFileName = config.extend || 'oauth';
     let modalPath = path.join(app.config.baseDir, `app/extend/${extendModelFileName}`);
-    if (!fs.existsSync(modalPath)) {
-       throw new Error("no extend model");
+
+    try {
+        const Model = require(modalPath);
+        const model = new Model(app);
+        app.oauthjwt = new OAuth2ServerBuilder(config, model, app.coreLogger);
+        app.coreLogger.info('[egg-oauth-jwt] init success');
+    } catch (e) {
+        app.coreLogger.error(`no extend model at app/extend/${extendModelFileName}.js`)
     }
-    const Model = require(modalPath);
-    const model = new Model(app);
-    app.oauthjwt = new OAuth2ServerBuilder(config, model, app.coreLogger);
-    app.coreLogger.info('[egg-oauth-jwt] init success');
 };

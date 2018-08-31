a45dd24d1013efadb9fc93848b0b086850fb9436
Tristan <tw949561391@hotmail.com>
2018-08-31 12:54:33 +0800

    publish

--- index.js
+++ index.js
@@ -3,4 +3,4 @@
 module.exports.OauthError = require('./lib/oauth/error/OauthError');
 module.exports.OauthModel = require('./lib/oauth/model/default-model');
 module.exports.JwtOauthModel = require('./lib/oauth/model/jwt-model');
-module.exports.RedisOauthModel = require('./lib/oauth/model/redis-modell');
+module.exports.RedisOauthModel = require('./lib/oauth/model/redis-model');
